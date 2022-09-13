8e2147e27add1059ea09e105addd9c36d559e94e
yoga wahyudi <yogawhy8@gmail.com>
2022-09-14 01:47:44 +0700

    fixed backdrop text color

--- src/component/Backdrop/UnderDevelopment.js
+++ src/component/Backdrop/UnderDevelopment.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import { Backdrop, Typography, Stack } from '@mui/material'
+import { useTheme } from "@mui/material/styles";
 
 const UnderDevelopment = () => {
+  const theme = useTheme()
     const [open, setOpen ] = useState(true)
     const hadleClick = () => {
         setOpen(false);
@@ -16,10 +18,10 @@ const UnderDevelopment = () => {
         <Stack spacing={5}
         alignItems="center"
             justifyContent="center">
-            <Typography variant="h5" align="center">
+            <Typography variant="h5" align="center" sx={{color: theme.palette.primary.contrastText}}>
                 This website is under development, some functions may not work
             </Typography>
-            <Typography variant="body1" align="center">
+            <Typography variant="body1" align="center" sx={{color: theme.palette.primary.contrastText}}>
                 (click anywhere to continue)
             </Typography>
         </Stack>
